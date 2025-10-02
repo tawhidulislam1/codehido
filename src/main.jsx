bba78b9833bb0ee2d17b54ac93883a34b9b8419b
unknown <designwithtawhid@gmail.com>
2025-10-02 16:51:15 +0600

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './styles/app.css'
+import './App.css'
+
 import Router from './Router/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
