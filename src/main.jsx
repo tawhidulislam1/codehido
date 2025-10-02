ac5d88e078d9301e73bf6d577b68ba238ec24505
unknown <designwithtawhid@gmail.com>
2025-10-02 16:49:54 +0600

    Add missing app.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './app.css'
+import './styles/app.css'
 import Router from './Router/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
